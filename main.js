59d4cc6ac3127a6c276f640eefdb706c8ed63a59
Rebecca Jackson <zimbecs@gmail.com>
2018-12-17 18:51:54 +1300

    input of numbers, clear and all clear are working

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 Javascript for the functions of the calculator 
  
 Created by: Rebecca Jackson as part of Dev Academy Sprint 5 challenges
-*/
-
-/*User Story #7: At any time, pressing the clear button clears the input and output values, and returns the calculator to its initialized state; 0 should be shown in the element with the id of display.
 
 User Story #8: As I input numbers, I should be able to see my input in the element with the id of display.
 
@@ -23,36 +20,102 @@ User Story #14: Pressing an operator immediately following = should start a new 
 User Story #15: My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).*/
 
 document.addEventListener('DOMContentLoaded', runCalculator)
-// Event listeners
+
+//global variables
+//stores the input from the user
+var input = [''];
+//string that stores current inputs
+var totalString;
+// validation checks
+var validationOp = ['+', '-', '/', '*'];
+var validationDot = ['.'];
+//numbers for validation
+var nums = [0,1,2,3,4,5,6,7,8,9];
+
 
 function runCalculator(){
-  //stores the input from the user
-  var input = [""];
-  //string that stores current inputs
-  var totalString;
-  // validation checks
-  var validationOp = ['+', '-', '/', '*'];
-  var validationDot = ['.'];
-  //numbers for validation
-  var nums = [0,1,2,3,4,5,6,7,8,9];
+  setListeners()
+}//runcalculator()
 
-  function getValue(input){
 
-  }
+function setListeners(){
+  //console.log('set listeners')
+}
 
-  function update(){
-    totalString =input.join('');
-    $('#display').html(totalString);
-  }
+// Event listeners
+$('button').on('click', function(){
+  console.log(this.id)
+ switch(this.id){
+  case 'one':
+    input.push('1');
+  break;
+  case 'two':
+    input.push('2');
+  break;
+  case 'three':
+    input.push('3');
+  break;
+  case 'four':
+   input.push('4');
+  break;
+  case 'five':
+    input.push('5');
+  break;
+  case 'six':
+    input.push('6');
+  break;
+  case 'seven':
+    input.push('7');
+  break;
+  case 'eight':
+    input.push('8');
+  break;
+  case 'nine':
+    input.push('9');
+  break;
+  case 'zero':
+    input.push('0');
+  break;
+  case 'decimal':
+    input.push('.');
+  break;
+  case 'clearOne':
+    input.pop();
+  break;
+  case 'clearAll':
+    input = [''];
+  break;
+  case 'add':
+    
+  break;
+  case 'minus':
+    
+  break;
+  case 'divide':
+    
+  break;
+  case 'multiply':
+    
+  break;
+  case 'multiply':
+    update();
+    getTotal();
+  break;
 
-  function getTotal(){
-    totalString = input.join('');
-    $('#display').html(eval(totalString));
-  }
+ }
+  update();
+});
 
-  
+function getValue(input){
 
 }
 
+function update(){
+  totalString = input.join('');
+  $('#display').html(totalString);
+}
 
-
+function getTotal(){
+  totalString = input.join('');
+  $('#display').html(eval(totalString));
+}
