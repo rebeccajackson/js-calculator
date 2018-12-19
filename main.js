cbb8a81541631e223c90871869aac42cb7fc062b
Rebecca Jackson <zimbecs@gmail.com>
2018-12-20 12:39:43 +1300

    started keypress event listeners

--- main.js
+++ main.js
@@ -13,6 +13,86 @@ function setup(){
   input = '0';
 }
 
+//Keyboard event listeners
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+  // if(event.which == 48){
+  //   input += 0;
+  //   $("#display").text(input);
+  // }
+  if(input === '0'){
+    input = '';
+  }
+ switch(event.which){
+  case 49:
+    input = input + '1';
+  break;
+  /*case 'two':
+    input = input + '2';
+  break;
+  case 'three':
+    input = input + '3';
+  break;
+  case 'four':
+   input = input + '4';
+  break;
+  case 'five':
+    input = input + '5';
+  break;
+  case 'six':
+    input = input + '6';
+  break;
+  case 'seven':
+    input = input + '7';
+  break;
+  case 'eight':
+    input = input + '8';
+  break;
+  case 'nine':
+    input = input + '9';
+  break;
+  case 'zero':
+    appendZero();
+  break;
+  case 'decimal':
+    appendDecimal();
+  break;
+  case 'clearOne':
+    input = input.slice(0, -1);
+  break;
+  case 'clearAll':
+    input = '0';
+  break;
+  case 'add':
+    appendOperator('+'); 
+  break;
+  case 'minus':
+    appendOperator('-');  
+  break;
+  case 'divide':
+    appendOperator('/'); 
+  break;
+  case 'multiply':
+    appendOperator('*'); 
+  break;
+  case 'equals':
+    getTotal();
+  break;*/
+ }
+update();
+});
+function processKeys(argument){
+  if(input === '0'){
+    input = '';
+  }
+ switch(argument){
+  case 'one':
+  case 48:
+    input = input + '1';
+  break;
+ }
+}
+
 // Event listeners
 $('button').on('click', function(){
   if(input === '0'){
